f8c9b91045dc1dcb68cdd6e0df58fc84871a053b
Andreas <andrew.eracleous@gmail.com>
2018-08-24 13:26:58 +0300

    Added Chromapolis website

---
+++ project/premium/chromapolis/js/main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($){   
+$("[data-toggle=popover]").each(function(i, obj) {
+
+$(this).popover({
+  html: true,
+  content: function() {
+    var id = $(this).attr('id')
+    return $('#popover-content-' + id).html();
+  }
+});
+
+});
+
+});    
